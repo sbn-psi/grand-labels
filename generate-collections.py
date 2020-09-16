4253c2f56a9d7d18691d330a0bcb6c6afdebaecb
Jesse Stone <jstone@psi.edu>
2020-09-16 16:40:22 -0700

    generate_collections now uses bundles.json

--- generate-collections.py
+++ generate-collections.py
@@ -8,15 +8,18 @@ ENVIRONMENT=Environment(loader=FileSystemLoader('templates'), lstrip_blocks=True
 TEMPLATE=ENVIRONMENT.get_template("collection.xml.jinja")
 
 def main():
-    spec = json.load(open("collections.json"))
-    for collection in spec:
-        bundle_id = collection["bundle_id"]
-        collection_id = collection["collection_id"]
+    bundles = json.load(open("bundles.json"))
 
+    for bundle in bundles:
+        bundle_id = bundle["bundle_id"]
+        
         os.makedirs(bundle_id, exist_ok=True)
-        output_path = os.path.join(bundle_id, "collection_" + collection_id + ".xml")
-        with open(output_path, "w") as output_file:
-            output_file.write(TEMPLATE.render(**collection))
+
+        for collection in bundle["collections"]:
+            collection_id = collection["collection_id"]
+            collection_output_path = os.path.join(bundle_id, "collection_" + collection_id + ".xml")
+            with open(collection_output_path, "w") as output_file:
+                output_file.write(TEMPLATE.render(bundle=bundle, collection=collection))
 
 if __name__ == "__main__":
     main()
