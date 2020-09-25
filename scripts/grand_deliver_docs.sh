e72c4599a3e78f469ecd9f476205070af4abe0db
Jesse Stone <jstone@psi.edu>
2020-09-25 16:21:57 -0700

    added a script to copy documents to the right place

---
+++ scripts/grand_deliver_docs.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+
+set -e
+mv temp/Dawn_GRaND_BGO_calibrated_data_processing.* dawn-grand-ancillary/document/
+mv temp/Dawn_GRaND_Ceres_bundle_description.* dawn-grand-ceres/document/
+mv temp/Dawn_GRaND_DC034_activity_report.* dawn-grand-cruise/document/
+mv temp/Dawn_GRaND_DC041_activity_report.*  dawn-grand-cruise/document/
+mv temp/Dawn_GRaND_Mars_Gravity_Assist_results.* dawn-grand-mars/document/
+mv temp/Dawn_GRaND_Mars_bundle_description.* dawn-grand-mars/document/
+mv temp/Dawn_GRaND_Vesta_bundle_description.* dawn-grand-vesta/document/
+mv temp/Dawn_GRaND_Vesta_encounter_timeline.* dawn-grand-vesta/browse/
+mv temp/Dawn_GRaND_Vesta_gamma_ray_spectra_by_subphase.* dawn-grand-vesta/browse/
+mv temp/Dawn_GRaND_cruise_bundle_description.* dawn-grand-cruise/document/
+mv temp/Dawn_GRaND_cruise_ops.* dawn-grand-cruise/document/
+mv temp/Dawn_GRaND_data_processing.* dawn-grand-mars/document/
+mv temp/Dawn_GRaND_ephemeris_pointing_geometry.* dawn-grand-ancillary/document/
+mv temp/Dawn_GRaND_instrument_description.* dawn-grand-ancillary/document/
+mv temp/Dawn_GRaND_raw_data_description.* dawn-grand-ancillary/document/
