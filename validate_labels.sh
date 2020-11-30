88dd8da8ca11098ccd807318b91b852b16733e1f
Jesse Stone <jstone@psi.edu>
2020-11-30 09:47:10 -0700

    Label delivery for 2020-11-30

--- validate_labels.sh
+++ validate_labels.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-find dawn-grand-* -name "collection*.xml" -exec ~/bin/validate-1.23.3/bin/validate '{}' \; | egrep '(FAIL)|(PASS)|(ERROR)'
 find dawn-grand-* -name "bundle*.xml" -exec ~/bin/validate-1.23.3/bin/validate '{}' \; | egrep '(FAIL)|(PASS)|(ERROR)'
+find dawn-grand-* -name "collection*.xml" -exec ~/bin/validate-1.23.3/bin/validate '{}' \; | egrep '(FAIL)|(PASS)|(ERROR)'
