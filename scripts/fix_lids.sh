427e2b5fb32b362310c112d49a54414a6a2bb64f
Jesse Stone <jstone@psi.edu>
2021-02-09 14:28:39 -0700

    corrected LIDs for livetime table

---
+++ scripts/fix_lids.sh
@@ -0,0 +1,5 @@
+
+find dawn-grand-ancillary -name "*.xml" -exec sed -i.bak \
+    -e 's/urn:nasa:pds:dawn-grand-vesta:document:dawn_grand_archive_description/urn:nasa:pds:dawn-grand-ancillary:document:dawn_grand_archive_description.pdf/' \
+    -e 's/urn:nasa:pds:dawn-grand-vesta:document:grd_l1b_ephemeris_pointing_geometry_v3/urn:nasa:pds:dawn-grand-ancillary:document:dawn_grand_ephemeris_pointing_geometry.pdf/' \
+    -e 's/urn:nasa:pds:dawn_grand:data:state_table/urn:nasa:pds:dawn-grand-ancillary:miscellaneous:grd_state_table/' '{}' \;
