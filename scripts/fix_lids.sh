db3730a1d2255dca1335d437677714eade5de029
Jesse Stone <jstone@psi.edu>
2021-02-09 14:40:01 -0700

    fixed EPG reference

--- scripts/fix_lids.sh
+++ scripts/fix_lids.sh
@@ -5,4 +5,5 @@ find dawn-grand-ancillary -name "*.xml" -exec sed -i.bak \
     -e 's/urn:nasa:pds:dawn_grand:data:state_table/urn:nasa:pds:dawn-grand-ancillary:miscellaneous:grd_state_table/' '{}' \;
 
 find dawn-grand-ceres -name "*.xml" -exec sed -i.bak \
+    -e 's/urn:nasa:pds:dawn-grand-ceres:document:grd_l1b_ephemeris_pointing_geometry_v3/urn:nasa:pds:dawn-grand-ancillary:document:dawn_grand_ephemeris_pointing_geometry.pdf/' \
     -e 's/urn:nasa:pds:dawn_grand:data:state_table/urn:nasa:pds:dawn-grand-ancillary:miscellaneous:grd_state_table/' '{}' \;
