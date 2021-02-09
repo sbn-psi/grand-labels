3953339a8c5bcc59ab1ecaf924931fd9d1b52920
Jesse Stone <jstone@psi.edu>
2021-02-09 14:43:30 -0700

    Fixed cruise state table references

--- scripts/fix_lids.sh
+++ scripts/fix_lids.sh
@@ -9,3 +9,6 @@ find dawn-grand-ceres -name "*.xml" -exec sed -i.bak \
     -e 's/urn:nasa:pds:dawn-grand-ceres:document:grd_l1b_bgo_data_processing_v4_1/urn:nasa:pds:dawn-grand-ancillary:document:dawn_grand_bgo_calibrated_data_processing.pdf/' \
     -e 's/urn:nasa:pds:dawn-grand-ceres:data_reduced:ceres_spc181019_0512_plt_1_00.stl/urn:nasa:pds:dawn-grand-ceres:miscellaneous:ceres_spc181019_0512_plt_1_00.stl/' \
     -e 's/urn:nasa:pds:dawn_grand:data:state_table/urn:nasa:pds:dawn-grand-ancillary:miscellaneous:grd_state_table/' '{}' \;
+
+find dawn-grand-cruise -name "*.xml" -exec sed -i.bak \
+    -e 's/urn:nasa:pds:dawn_grand:data:state_table/urn:nasa:pds:dawn-grand-ancillary:miscellaneous:grd_state_table/' '{}' \;
